01fe1b5755224cb83d77787c015dca2e449f97d3
PJannie <31662113+PJannie@users.noreply.github.com>
2021-03-18 20:50:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
- <?php
-$servername = "localhost";
-$username = "root"; // default username for localhost is root
-$password = ""; // default password for localhost is empty
-$dbname = "crud"; // database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-?> 
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>

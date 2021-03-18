7ccf00eb741ba32ba052cff0febc33f12e9cc11f
PJannie <31662113+PJannie@users.noreply.github.com>
2021-03-18 09:47:06 +0100

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php 
+include "config.php";
+
+// if the 'id' variable is set in the URL, we know that we need to edit a record
+if (isset($_GET['id'])) {
+	$user_id = $_GET['id'];
+
+	// write delete query
+	$sql = "DELETE FROM `users` WHERE `id`='$user_id'";
+	$sql2 = "ALTER TABLE `users` Auto_increment = 1";
+
+	// Execute the query
+
+	$result = $conn->query($sql);
+	$result = $conn->query($sql2);
+
+	if ($result == TRUE) {
+		echo "Record deleted successfully.";
+		header("Location: index.php");
+		exit();
+	}else{
+		echo "Error:" . $sql . "<br>" . $conn->error;
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,119 @@
+<?php 
+include "config.php";
+
+// if the form's submit button is clicked, we need to process the form
+	if (isset($_POST['submit'])) {
+		// get variables from the form
+		$first_name = $_POST['firstname'];
+		$last_name = $_POST['lastname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$gender = $_POST['gender'];
+
+		//write sql query
+
+		$sql = "INSERT INTO `users`(`firstname`, `lastname`, `email`, `password`, `gender`) VALUES ('$first_name','$last_name','$email','$password','$gender')";
+
+		// execute the query
+
+		$result = $conn->query($sql);
+
+		if ($result == TRUE) {
+			echo "New record created successfully.";
+		}else{
+			echo "Error:". $sql . "<br>". $conn->error;
+		}
+
+		$conn->close();
+
+	}
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Signup Form</h2>
+
+<form action="" method="POST">
+  <fieldset>
+    <legend>Personal information:</legend>
+    First name:<br>
+    <input type="text" name="firstname">
+    <br>
+    Last name:<br>
+    <input type="text" name="lastname">
+    <br>
+    Email:<br>
+    <input type="email" name="email">
+    <br>
+    Password:<br>
+    <input type="password" name="password">
+    <br>
+    Gender:<br>
+    <input type="radio" name="gender" value="Male">Male
+    <input type="radio" name="gender" value="Female">Female
+    <br><br>
+    <input type="submit" name="submit" value="submit">
+  </fieldset>
+</form>
+
+
+<?php 
+include "config.php";
+
+//write the query to get data from users table
+
+$sql = "SELECT * FROM users";
+
+//execute the query
+
+$result = $conn->query($sql);
+
+
+?>
+
+
+<div class="container">
+		<h2>users</h2>
+<table class="table">
+	<thead>
+		<tr>
+		<th>ID</th>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Email</th>
+		<th>Gender</th>
+		<th>Action</th>
+	</tr>
+	</thead>
+	<tbody>	
+		<?php
+			if ($result->num_rows > 0) {
+				//output data of each row
+				while ($row = $result->fetch_assoc()) {
+		?>
+
+					<tr>
+					<td><?php echo $row['id']; ?></td>
+					<td><?php echo $row['firstname']; ?></td>
+					<td><?php echo $row['lastname']; ?></td>
+					<td><?php echo $row['email']; ?></td>
+					<td><?php echo $row['gender']; ?></td>
+					<td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+					</tr>	
+					
+		<?php		}
+			}
+		?>
+	        	
+	</tbody>
+</table>
+	</div>
+
+</body>
+</html>
